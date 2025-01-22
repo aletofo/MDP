ca147f66486f064cafd8f0177f4a4542f98da7b0
ALESSIO TOFONI <atofoni6@gmail.com>
2025-01-22 17:28:16 +0100

    almost finished, need to fix the codes length

--- Exams/20140929 - HUFFMAN4decimal/HUFFMAN4decimal/main.cpp
+++ Exams/20140929 - HUFFMAN4decimal/HUFFMAN4decimal/main.cpp
@@ -11,25 +11,80 @@
 #include <ranges>
 #include <algorithm>
 
-template<typename T>
-struct huffman_tree {
+struct huffman {
 	struct node {
-		T sym_;
 		int num_;
 		size_t freq_;
 		node* left_;
 		node* right_;
+		uint8_t sym_;
+		uint8_t symlen_;
 	};
+
+	struct nodeptr_less {
+		bool operator()(node* a, node* b) {
+			return a->freq_ > b->freq_;
+		}
+	};
+
+	std::vector<std::unique_ptr<node>> nodes_;
 
 	template<typename... Ts>
 	node* make_node(Ts... args) {
-		node* n;
+		nodes_.emplace_back(std::make_unique<node>(std::forward<Ts>(args)...));
+		return nodes_.back().get();
 	}
 
-	void operator() (std::unordered_map<int, size_t> f) {
-		for (auto [key, value] : f) {
-			node* n = make_node(key, value);
+
+	void generate_codes(node* n, uint8_t buffer, uint8_t len) {
+		if (n->left_ != nullptr && n->right_ != nullptr) {
+			generate_codes(n->left_, (buffer << 1), ++len);
+			generate_codes(n->right_, (buffer << 1) | 1, ++len);
 		}
+		else {
+			n->sym_ = buffer;
+			n->symlen_ = len;
+		}
+	}
+
+	std::vector<std::tuple<uint8_t, uint64_t, uint8_t>> getnodes() { 
+		std::vector<std::tuple<uint8_t, uint64_t, uint8_t>> v;
+
+		for (int i = 0; i < nodes_.size(); ++i) {
+			if (nodes_[i]->num_ == 0) {
+				continue;
+			}
+			auto t = std::make_tuple(nodes_[i]->symlen_, nodes_[i]->num_, nodes_[i]->sym_);
+			v.push_back(t);
+		}
+		std::sort(v.begin(), v.end(), [](const auto& t1, const auto& t2) {
+			return std::get<0>(t1) < std::get<0>(t2); // Confronta il primo valore
+			});
+		
+		return v; 
+	}
+
+	huffman(std::unordered_map<int, size_t> f) {
+		std::vector<node*> v;
+
+		for (auto [key, freq] : f) {
+			node* n = make_node(key, freq);
+			v.push_back(n);
+		}
+		std::ranges::sort(v.begin(), v.end(), nodeptr_less{});
+
+		while (v.size() > 1) {
+			node* n1 = v.back();
+			v.pop_back();
+			node* n2 = v.back();
+			v.pop_back();
+
+			node* n = make_node(0, n1->freq_ + n2->freq_, n1, n2);
+			auto pos = std::ranges::lower_bound(v, n, nodeptr_less{});
+			v.insert(pos, n);
+		}
+		node* root = v.back();
+		generate_codes(root, 0, 0);
 	}
 
 };
@@ -103,6 +158,13 @@ int main(int argc, char* argv[]) {
 
 	std::vector<int> v = read_inputfile(is);
 	std::unordered_map<int, size_t> f = frequencies(v);
+	huffman h(f);
+	std::vector<std::tuple<uint8_t, uint64_t, uint8_t>> nodes = h.getnodes();
+
+	for (std::tuple<uint8_t, uint64_t, uint8_t> t : nodes) {
+		os << std::get<0>(t);
+		os << std::get<1>(t);
+	}
 
 	return EXIT_SUCCESS;
 }
